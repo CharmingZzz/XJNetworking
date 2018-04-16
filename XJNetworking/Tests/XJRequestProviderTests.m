4160b8e8387f06a46d97e596931ef1f578b0b5bc
CharmingZzz <17600026862@163.com>
2018-04-16 14:19:25 +0800

    Test Code Commit

--- XJNetworking/Tests/XJRequestProviderTests.m
+++ XJNetworking/Tests/XJRequestProviderTests.m
@@ -14,6 +14,7 @@
 @property (nonatomic,strong)XJRequestProvider *provider;
 @property (nonatomic,strong)id commonSourceMock;
 @property (nonatomic,strong)id pluginMock;
+@property (nonatomic,assign)BOOL pluginTestOpen;
 
 @end
 
@@ -23,13 +24,12 @@
     [super setUp];
     
     self.provider = [XJRequestProvider defaultProvider];
+    self.pluginTestOpen = NO;
     
     // mock XJRequestProviderCommonSource
     id apiMock = OCMProtocolMock(@protocol(XJRequestProviderCommonSource));
     OCMStub([apiMock baseURL]).andReturn(@"https://httpbin.org");
-    OCMStub([apiMock methodname]).andReturn(@"ip");
     OCMStub([apiMock parameters]).andReturn(@{});
-    OCMStub([apiMock requestType]).andReturn(XJRequestProviderRequestTypeGet);
     OCMStub([apiMock taskType]).andReturn(XJRequestProviderTaskTypeRequest);
     OCMStub([apiMock requestSerialization]).andReturn([AFHTTPRequestSerializer serializer]);
     OCMStub([apiMock responseSerialization]).andReturn([AFJSONRequestSerializer serializer]);
@@ -38,8 +38,9 @@
     // mock
     id pluginMock = OCMProtocolMock(@protocol(XJRequestProviderSourcePlugin));
     OCMStub([pluginMock shouldSendApiWithParams:[OCMArg any] caller:[OCMArg any]]).andReturn(YES);
-    OCMStub([pluginMock willSendApiWithParams:[OCMArg any]]).andReturn(@{});
-    OCMStub([pluginMock willSendApiWithRequest:[OCMArg any]]).andReturn([NSURLRequest new]);
+    OCMStub([pluginMock willSendApiWithParams:[OCMArg any]]).andCall(self,@selector(mock_willSendApiWithParams:));
+    OCMStub([pluginMock willSendApiWithRequest:[OCMArg any]]).andCall(self,@selector(mock_willSendApiWithRequest:));
+    OCMStub([self.pluginMock afterSendApiWithParams:[OCMArg any] caller:[OCMArg any]]).andCall(self,@selector(mock_afterSendApiWithParams:caller:));
     OCMStub([pluginMock beforeApiSuccessWithResponse:[OCMArg any]]).andReturn(YES);
     OCMStub([pluginMock afterApiSuccessWithResponse:[OCMArg any] caller:[OCMArg any]]);
     OCMStub([pluginMock beforeApiFailureWithError:[OCMArg any]]).andReturn(YES);
@@ -56,13 +57,19 @@
     self.pluginMock = nil;
 }
 
-- (void)testRequest {
+- (void)testRequestGet {
+    OCMStub([self.commonSourceMock methodname]).andReturn(@"get");
+    [self startRequest:self.commonSourceMock];
+}
 
+- (void)testRequestPost {
+    OCMStub([self.commonSourceMock requestType]).andReturn(XJRequestProviderRequestTypePost);
+    OCMStub([self.commonSourceMock methodname]).andReturn(@"post");
     [self startRequest:self.commonSourceMock];
 }
 
 - (void)testPlugins1 {
-    
+    OCMStub([self.commonSourceMock methodname]).andReturn(@"get");
     OCMStub([self.commonSourceMock plugins]).andReturn(@[self.pluginMock]);
 
     [self startRequest:self.commonSourceMock];
@@ -70,16 +77,89 @@
     OCMVerify([self.pluginMock shouldSendApiWithParams:[OCMArg any] caller:[OCMArg any]]);
     OCMVerify([self.pluginMock willSendApiWithParams:[OCMArg any]]);
     OCMVerify([self.pluginMock willSendApiWithRequest:[OCMArg any]]);
+    OCMVerify([self.pluginMock afterSendApiWithParams:[OCMArg any] caller:[OCMArg any]]);
     OCMVerify([self.pluginMock beforeApiSuccessWithResponse:[OCMArg any]]);
     OCMVerify([self.pluginMock afterApiSuccessWithResponse:[OCMArg any] caller:[OCMArg any]]);
 }
 
-- (void)testCancelleable
+- (void)testPlugins2 {
+    OCMStub([self.commonSourceMock methodname]).andReturn(@"get");
+    OCMStub([self.commonSourceMock plugins]).andReturn(@[self.pluginMock]);
+    
+    self.pluginTestOpen = YES;
+    
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"test Async requset Plugins"];
+    
+    [self.provider requestWithSource:self.commonSourceMock from:self
+                                                                 success:^(XJURLResponse *response) {
+                                                                     XCTAssertEqualObjects(response.request.allHTTPHeaderFields[@"testplugin"], @"testplugin",@"plugin is already add testplugin key to httpheader");
+                                                                     NSLog(@"----%@-----",response.request.xj_requestParams);
+                                                                     XCTAssertEqualObjects(response.request.xj_requestParams[@"testplugin"], @"testplugin",@"plugin is already add testplugin key to params");
+                                                                     [expectation fulfill];
+                                                                 } failure:^(NSError *error) {
+                                                                     XCTAssertNil(error,@"----request should be success---");
+                                                                     [expectation fulfill];
+                                                                 }];
+    [self waitForExpectationsWithTimeout:8.f handler:nil];
+    
+}
+
+- (void)testCancelleable1
 {
+    OCMStub([self.commonSourceMock methodname]).andReturn(@"get");
     XJRequestCancellable *cancellable = [self startRequest:self.commonSourceMock];
     XCTAssertTrue(cancellable.isCancelled,@"request is already cancel");
 }
 
+- (void)testCancelleable2
+{
+    OCMStub([self.commonSourceMock methodname]).andReturn(@"delay/10");
+    OCMStub([self.commonSourceMock plugins]).andReturn(@[self.pluginMock]);
+
+    XCTestExpectation *expectation = [self expectationWithDescription:@"test Async requset Cancelleable"];
+
+    XJRequestCancellable *cancellable = [self.provider requestWithSource:self.commonSourceMock from:self
+                                                                 success:^(XJURLResponse *response) {
+                                                                     XCTAssertNil(response,@"----request should be failure---");
+                                                                     [expectation fulfill];
+                                                                 } failure:^(NSError *error) {
+                                                                     XCTAssertEqual(error.code, NSURLErrorCancelled,@"error code should be NSURLErrorCancelled");
+                                                                     [expectation fulfill];
+                                                                 }];;
+
+    [cancellable cancel];
+    
+    [self waitForExpectationsWithTimeout:8.f handler:nil];
+    
+    OCMVerify([self.pluginMock beforeApiFailureWithError:[OCMArg any]]);
+    OCMVerify([self.pluginMock afterApiFailureWithError:[OCMArg any] caller:[OCMArg any]]);
+}
+
+- (NSDictionary *)mock_willSendApiWithParams:(NSDictionary *)params
+{
+    if(self.pluginTestOpen){
+        NSMutableDictionary *muParams = [NSMutableDictionary dictionaryWithDictionary:params];
+        [muParams setObject:@"testplugin" forKey:@"testplugin"];
+        return [muParams copy];
+    }
+    return params;
+}
+
+- (NSURLRequest *)mock_willSendApiWithRequest:(NSURLRequest *)request
+{
+    if (self.pluginTestOpen){
+        NSMutableURLRequest *muRequest = [request mutableCopy];
+        muRequest.allHTTPHeaderFields = @{@"testplugin":@"testplugin"};
+        return [muRequest copy];
+    }
+    return request;
+}
+
+- (void)mock_afterSendApiWithParams:(NSDictionary *)params caller:(id)caller
+{
+    XCTAssertEqualObjects(caller, self,@"caller should be equal to self");
+}
 
 - (XJRequestCancellable *)startRequest:(id <XJRequestProviderCommonSource>)source {
     
@@ -91,13 +171,11 @@
         XCTAssertNotNil(response.request,@"----success request couldn't be nil---");
         [expectation fulfill];
     } failure:^(NSError *error) {
-        XCTAssertNotNil(error,@"----failure error couldn't be nil---");
+        XCTAssertNil(error,@"----request should be success---");
         [expectation fulfill];
     }];
     
-    [self waitForExpectationsWithTimeout:10.f handler:^(NSError * _Nullable error) {
-        
-    }];
+    [self waitForExpectationsWithTimeout:10.f handler:nil];
     return cancellable;
 }
 
